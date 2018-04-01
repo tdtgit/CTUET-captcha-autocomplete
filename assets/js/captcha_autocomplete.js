25469574d137918555a03478faa518e3433c7b1d
Anh Tuấn <mrsugarvn@gmail.com>
2018-04-01 22:09:07 +0700

    Remove unused jQuery

--- assets/js/captcha_autocomplete.js
+++ assets/js/captcha_autocomplete.js
@@ -1,11 +1,16 @@
-function solveCaptcha(){
-    $("input#txtSercurityCode").val(" ");
-    $("input#txtSercurityCode1").val(" ");
-    $("input#txtSecurityCodeValue").val("7215ee9c7d9dc229d2921a40e899ec5f");
-    $("input#txtSecurityCodeValue1").val("7215ee9c7d9dc229d2921a40e899ec5f");
+function solveCaptcha(elements){
+    for(var $ele in elements){
+        if(elements.hasOwnProperty($ele) && document.getElementById($ele) != null){
+            document.getElementById($ele).value = elements[$ele];
+        }
+    }
 }
 
-jQuery(function(){
-    // Lấy captcha khi vào trang
-    solveCaptcha();
-});
+window.onload = function(){
+    solveCaptcha({
+        'txtSercurityCode':         ' ',
+        'txtSercurityCode1':        ' ',
+        'txtSecurityCodeValue':     '7215ee9c7d9dc229d2921a40e899ec5f',
+        'txtSecurityCodeValue1':    '7215ee9c7d9dc229d2921a40e899ec5f'
+    })
+}
