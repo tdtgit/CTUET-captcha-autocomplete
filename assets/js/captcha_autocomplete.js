88ab45c86510f7362ee34763a4a4b592a6c80d7d
Anh Tuấn <mrsugarvn@gmail.com>
2018-04-01 22:33:49 +0700

    Release 1.1

--- assets/js/captcha_autocomplete.js
+++ assets/js/captcha_autocomplete.js
@@ -6,11 +6,21 @@ function solveCaptcha(elements){
     }
 }
 
-window.onload = function(){
-    solveCaptcha({
-        'txtSercurityCode':         ' ',
-        'txtSercurityCode1':        ' ',
-        'txtSecurityCodeValue':     '7215ee9c7d9dc229d2921a40e899ec5f',
-        'txtSecurityCodeValue1':    '7215ee9c7d9dc229d2921a40e899ec5f'
-    })
+var elements = {
+    'txtSercurityCode':         ' ',
+    'txtSercurityCode1':        ' ',
+    'txtSecurityCodeValue':     '7215ee9c7d9dc229d2921a40e899ec5f',
+    'txtSecurityCodeValue1':    '7215ee9c7d9dc229d2921a40e899ec5f'
 }
+
+window.onload = function(){
+    solveCaptcha(elements);
+}
+
+document.body.addEventListener("click", function(e) {
+    console.log(e);
+    if(e && e.target.id == 'imgRefresh'){
+        solveCaptcha();
+        e.preventDefault();
+    }
+});
