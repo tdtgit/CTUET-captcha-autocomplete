f6b3a24b6de0c5b9ea4c2a6dad824c0de5353051
Argus Duong <mrsugarvn@gmail.com>
2020-07-05 18:03:12 +0700

    Linting and support non-HTTPs site
    Dunno why they're still using HTTP site

--- assets/js/captcha_autocomplete.js
+++ assets/js/captcha_autocomplete.js
@@ -1,26 +1,26 @@
-function solveCaptcha(elements){
-    for(var $ele in elements){
-        if(elements.hasOwnProperty($ele) && document.getElementById($ele) != null){
-            document.getElementById($ele).value = elements[$ele];
-        }
+function solveCaptcha(elements) {
+  for (var $ele in elements) {
+    if (elements.hasOwnProperty($ele) && document.getElementById($ele) != null) {
+      document.getElementById($ele).value = elements[$ele];
     }
+  }
 }
 
 var elements = {
-    'txtSercurityCode':         ' ',
-    'txtSercurityCode1':        ' ',
-    'txtSecurityCodeValue':     '7215ee9c7d9dc229d2921a40e899ec5f',
-    'txtSecurityCodeValue1':    '7215ee9c7d9dc229d2921a40e899ec5f'
-}
+  'txtSercurityCode': ' ',
+  'txtSercurityCode1': ' ',
+  'txtSecurityCodeValue': '7215ee9c7d9dc229d2921a40e899ec5f',
+  'txtSecurityCodeValue1': '7215ee9c7d9dc229d2921a40e899ec5f'
+};
 
-window.onload = function(){
-    solveCaptcha(elements);
-}
+window.onload = function() {
+  solveCaptcha(elements);
+};
 
 document.body.addEventListener("click", function(e) {
-    console.log(e);
-    if(e && e.target.id == 'imgRefresh'){
-        solveCaptcha();
-        e.preventDefault();
-    }
+  console.log(e);
+  if (e && e.target.id == 'imgRefresh') {
+    solveCaptcha();
+    e.preventDefault();
+  }
 });
